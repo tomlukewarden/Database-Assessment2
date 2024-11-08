b088d234bad7d80128b9ab3cf53ae7c92f06c36c
seb-navarro <navarrosebastian154@gmail.com>
2024-11-08 12:52:13 +0000

    added function to change text on button click

---
+++ pages/staff/dashboard/staff_dashboard.js
@@ -0,0 +1,8 @@
+function changetext() {
+    var x = document.getElementById("change");
+    if (x.innerHTML === "NO") {
+      x.innerHTML = "YES";
+    } else {
+      x.innerHTML = "NO";
+    }
+  }
