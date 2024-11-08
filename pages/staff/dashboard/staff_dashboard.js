9a1198e24dde0fa7b70a55afc7b778aa10e88e84
seb-navarro <navarrosebastian154@gmail.com>
2024-11-08 13:18:24 +0000

    formated clock in and out button

--- pages/staff/dashboard/staff_dashboard.js
+++ pages/staff/dashboard/staff_dashboard.js
@@ -1,8 +1,19 @@
-function changetext() {
+function changestatus() {
     var x = document.getElementById("change");
     if (x.innerHTML === "NO") {
       x.innerHTML = "YES";
+      x.className = "text-success"
     } else {
       x.innerHTML = "NO";
+      x.className = "text-danger"
+    }
+
+    var y = document.getElementById("changebutton");
+    if(y.innerHTML === "Clock In") {
+        y.innerHTML = "Clock Out"
+        y.className = "bg-danger"
+    } else{
+        y.innerHTML = "Clock In"
+        y.className = "bg-success"
     }
   }
