a6f07e75c314162787a71fd6766ee9dc4ff05214
tom <141278466+tomlukewarden@users.noreply.github.com>
2024-11-11 13:02:23 +0000

    changing dash to php

--- pages/db-admin-view/dashboard/dash.php
+++ pages/db-admin-view/dashboard/dash.php
@@ -13,7 +13,6 @@
 </head>
 
 <body>
-
     <nav class="navbar navbar-dark bg-dark fixed-top p-3">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Espresso Express Dashboard</a>
@@ -256,6 +255,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
+
 </body>
 
 </html>
