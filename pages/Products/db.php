eaae434fa6e781c0546a339d8df1b6cb0e5ba9b5
Callum McDonald <callummcd1@gmail.com>
2024-11-13 15:30:10 +0000

    update

--- pages/Products/db.php
+++ pages/Products/db.php
@@ -1,10 +1,9 @@
 <?php 
-$host = "espresso-express.cruet0gdarxg.us-east-1.rds.amazonaws.com"; 
-$username = "aidan"; 
-$password = "express123"; 
-$database = "espresso-express"; 
+$host = "labdatabase1.cvhoavzabfmd.us-east-1.rds.amazonaws.com"; 
+$username = "callum"; 
+$password = "changemee20486"; 
+$database = "LabDatabase1"; 
 $mysql = new PDO("mysql:host=".$host.";dbname=".$database, 
 $username, $password); 
 echo "DB linked Successfully! <br>"; 
-
 ?> 
--- pages/Products/index.php
+++ pages/Products/index.php
@@ -1,2 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+
 <a href="db.php">Link to DB.PHP file</a>
+
+<?php session_start();
+// store session data
+if(isset($_SESSION['views'])){
+$_SESSION['views'] = $_SESSION['views'] + 1;
+}
+else{
+$_SESSION['views'] = 1;
+}
+// display the session variable
+echo "The number of views is ".$_SESSION['views'];
+?>
+
+
+<?php
+// Include the database connection
+include 'db.php';
+$query = "SELECT * FROM Customer";
+$stmt = $mysql->prepare($query);
+$stmt->execute();
+$result = $stmt->fetchAll();
+?>
+
+<div class="container">
+
+<!-- HTML with PHP embedded in the table -->
+<table class="table table-striped justify-content-evenly">
+    <thead >
+        <tr class="table-primary">
+            <td>Firstname</td>
+            <td>Surname</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach($result as $row) {
+        echo "<td>".$row['first_name']."</td>";
+        echo "<td>".$row['first_name']."</td></tr>";
+        }
+        ?>
+    </tbody>
+</table>
+</div>
+
+<br>
+<br>
+
+
+
+</body>
+</html>
+
+
 
