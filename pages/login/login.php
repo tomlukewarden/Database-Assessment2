a84e5cb7abc9f9bb932170c8ad91758a6a546003
Callum McDonald <callummcd1@gmail.com>
2024-11-15 16:29:18 +0000

    added functionality ot login.php to open new tab with staff view of products when form is submitted

--- pages/login/login.php
+++ pages/login/login.php
@@ -49,7 +49,7 @@ $_SESSION["password"] = $_POST["password"];
                                 <label for="pass">Password</label>
                                 <input type="password" class="form-control" id="pass" placeholder="Password" name="password">
                             </div>
-                            <button type="submit" value="submit" name="submit" class="btn btn-primary my-2">Login</button>
+                            <button id="id1" type="submit" value="submit" name="submit" class="btn btn-primary my-2">Login</button>
                         </form>
                         <a href="#" class="card-link">Customer Page</a>
                     </div>
@@ -63,5 +63,5 @@ $_SESSION["password"] = $_POST["password"];
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
     crossorigin="anonymous"></script>
-
+<script src="form.js"></script>
 </html>
