4a3ee76c521384e932848fb923435f8646b7b36f
Callum McDonald <callummcd1@gmail.com>
2024-11-20 19:07:24 +0000

    deleted all outdated files for login and profile

--- pages/login/form.js
+++
@@ -1,9 +0,0 @@
-
-
-document.getElementById("id1").addEventListener("click", openStaffView);
-
-function openStaffView() {
-    window.open("./Products-staffView.php")
-}
-
-//link may not work in this folder - i have not tested it. re-do  path to staff view of products or change to staff profile.
