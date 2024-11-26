81c880ee67d0a985c4fc78c2d0870a00d5d3beb5
Callum McDonald <callummcd1@gmail.com>
2024-11-26 13:36:43 +0000

    Current version of products staff view

--- pages/Products/Products-staffView.php
+++ pages/Products/Products-staffView.php
@@ -20,7 +20,7 @@
 
     <nav class="navbar navbar-dark bg-dark fixed-top p-3 mb-3">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Espresso Express</a>
+            <a class="navbar-brand" href="#">Espresso Express Staff</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
                 aria-label="Toggle navigation">
@@ -66,40 +66,94 @@
 
                 </div>
                 
+                <!-- <form name="Table Properties" method="post" action="">
+                    Order by price
+                    <button type="submit" name="sort_price_asc" class="button" value="1"> Sort Price ASC</button>
+                    <button type="submit" name="sort_price_desc" class="button" value="1"> Sort Price DESC</button>
+                </form> -->
+
+
+                <form name="table_properties" method="post" action="">
+                    <label for="sort">Sort By:</label>
+                        <!-- DROPDOWN FOR SORTING THE TABLE -->
+                        <select class="btn btn-secondary" name="sort" id="sort">
+                            <option value="sort_id_asc">ID Asc</option>
+                            <option value="sort_id_desc">ID Desc</option>
+                            <option value="sort_price_asc">Price Asc</option>
+                            <option value="sort_price_desc">Price Desc</option>
+                            <option value="sort_name_asc">Name Asc</option>
+                            <option value="sort_name_desc">Name Desc</option>
+                        </select> 
+                    <button class="btn btn-secondary mx-3" type="submit">Sort</button>
+                </form>
+
                 <!-- TABLE OF PRODUCTS -->
                     <table class="table table-striped table-hover">
                     <thead>
                         <tr>
                             <th scope="col-md">#</th>
                             <th scope="col-md">Product Name</th>
-                            <th scope="col-md">Product Description</th>
-                            <th scope="col-md">Image</th>
+                            <th scope="col-md">Price</th>
                         </tr>
                     </thead>
+
                     <tbody>
-                        <tr>
-                            <th>1</th>
-                            <td>coffee 1</td>
-                            <td>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero porro minus dignissimos, atque earum praesentium quam officia quibusdam ullam tempora ducimus veritatis consectetur illum sit omnis, doloribus soluta debitis laudantium?</td>
-                            <td><img src="https://picsum.photos/200" alt="placeholder"></td>
-                            <td>Supplier:  </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>coffee 2</td>
-                            <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias repellendus aut voluptatibus atque! Tempore obcaecati, architecto accusamus explicabo aspernatur minus voluptatibus velit maxime necessitatibus odio quisquam ipsum ratione veritatis debitis?</td>
-                            <td><img src="https://picsum.photos/200" alt="placeholder"></td>
-                            <td>Supplier:  
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>coffee 3</td>
-                            <td>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi nam aut neque fugit incidunt, cupiditate reiciendis officia, corrupti rerum nesciunt quis nihil omnis veritatis facilis quidem quibusdam, quaerat ab.</td>
-                            <td><img src="https://picsum.photos/200" alt="placeholder"></td>
-                            <td>Supplier:  
-                            </td>
-                        </tr>
+                        
+                    <?php
+                        // CONNECT TO DATABASE
+                        include 'db.php';
+
+                        // DEFAULT QUERY
+                        $sql = "SELECT * FROM product";
+
+                        // RUN WHEN FORM IS SUBMITTED
+                        if (isset($_POST['sort'])) {
+                            $sortOption = $_POST['sort'];
+
+                            //SET QUERY BASED ON WHAT IS SUBMITTED IN FORM.
+                            switch ($sortOption) {
+                                case 'sort_id_asc':
+                                    $sql = "SELECT * FROM product ORDER BY product_id ASC";
+                                    break;
+                                case 'sort_id_desc':
+                                    $sql = "SELECT * FROM product ORDER BY product_id DESC";
+                                    break;
+                                case 'sort_price_asc':
+                                    $sql = "SELECT * FROM product ORDER BY price ASC";
+                                    break;
+                                case 'sort_price_desc':
+                                    $sql = "SELECT * FROM product ORDER BY price DESC";
+                                    break;
+                                case 'sort_name_asc':
+                                    $sql = "SELECT * FROM product ORDER BY product_name ASC";
+                                    break;
+                                case 'sort_name_desc':
+                                    $sql = "SELECT * FROM product ORDER BY product_name DESC";
+                                    break;
+                                default:
+                                    $sql = "SELECT * FROM product";
+                                    break;
+                            }
+                        }
+
+                        $result = mysqli_query($conn, $sql);
+
+                        if ($result) {
+                            // DISPLAY SORTED TABLE
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo "<tr>";
+                                echo "<td>" . $row['product_id'] . "</td>";
+                                echo "<td>" . $row['product_name'] . "</td>";
+                                echo "<td>£" .$row['price'] . ".00</td>";
+                                echo "</tr>";
+                            }
+                        } else {
+                            // ERROR MSG
+                            echo "<tr><td>Error: " . mysqli_error($conn) . "</td></tr>";
+                        }
+                    ?>
+
+
                     </tbody>
                 </table>
                 
@@ -109,12 +163,6 @@
         <br>
         <p class="text-light position-absolute start-50 top-50 translate-middle">&copy; Espresso Express</p>
         <br>
-        <!-- <div class="icons container-fluid">
-        <a href="https://github.com/UniversityOfDundee-Computing/cw1-web-development-project-sca" target="_blank"><i
-                class="fa-brands fa-github-square translate-middle h3 mx-3"></i></a>
-        <a href="https://en.wikipedia.org/wiki/RollerCoaster_Tycoon" target="_blank"><i
-                class="fa-brands fa-wikipedia-w translate-middle h3 mx-3"></i></a>
-    </div> -->
     </footer>
 
 </body>
