25006c4b2285afc6bbe33a5c7eb3c08312e2a3a9
tom <141278466+tomlukewarden@users.noreply.github.com>
2024-11-26 14:29:25 +0000

    pathways working

--- pages/db-admin/pages/tickets.php
+++ pages/db-admin/pages/tickets.php
@@ -13,9 +13,49 @@
 
 <body>
 <?php 
-include '../components/navbar.php';
 include '../db.php'; 
 ?>
+
+<nav class="navbar navbar-dark bg-dark fixed-top p-3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Espresso Express Dashboard</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+                aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dashboard Menu</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+                        aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item"><a class="nav-link active" href="../dash.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../../login/profile.php">Profile</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Admin Tools</a>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item" href="./requests.php">Requests <span class="badge text-bg-secondary">4</span></a></li>
+                                <li><a class="dropdown-item" href="./users.php">Users</a></li>
+                                <li><a class="dropdown-item" href="./transactions.php">Transactions</a></li>
+                                <li><a class="dropdown-item" href="#">Products</a></li>
+                                <li><a class="dropdown-item" href="../../Products/Products-staffView.php">Stock</a></li>
+                                <li><a class="dropdown-item" href="#">Orders</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Reports</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex mt-3" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
 
     <main class="mt-5 pt-4">
         <div class="container-fluid">
