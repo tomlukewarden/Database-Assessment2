04da717dd5e8af091a52cc75799ec5483ea09974
seb-navarro <navarrosebastian154@gmail.com>
2024-11-26 17:37:19 +0000

    fixed navbar

--- pages/staff/staff_dashboard.php
+++ pages/staff/staff_dashboard.php
@@ -75,14 +75,9 @@ if (isset($_POST['submit'])) {
                     </div>
                     <div class="offcanvas-body">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <li class="nav-item"><a class="nav-link" href="staff_profile.php">Profile</a></li>
                             <li class="nav-item"><a class="nav-link active" href="staff_dashboard.php">Staff Dashboard</a></li>
-                            <li class="" nav-item><a class="nav-link" href="staff_products.php">Products</a></li>
+                            <li class="nav-item"><a class="nav-link" href="staff_products.php">Products</a></li>
                         </ul>
-                        <form class="d-flex mt-3" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-success" type="submit">Search</button>
-                        </form>
                     </div>
                 </div>
             </div>
