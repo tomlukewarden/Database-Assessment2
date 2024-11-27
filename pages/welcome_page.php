1df0db5c9fb6266a1b126600fc2b37ea4af26a56
seb-navarro <navarrosebastian154@gmail.com>
2024-11-27 14:17:54 +0000

    edited php on welcome page

--- pages/welcome_page.php
+++ pages/welcome_page.php
@@ -52,53 +52,51 @@ session_start();
                         echo "<a class='navbar-brand' href='#'>Espresso Express Assistant Manager</a>";
                     }elseif($_SESSION['type'] == 'barista'){
                         echo "<a class='navbar-brand' href='#'>Espresso Express Staff</a>";
+                    }elseif($_SESSION['type'] == 'loyal'){
+                        echo "<a class='navbar-brand' href='#'>Espresso Express Loyal Customer Dashboard</a>";
+                    }
+                    if ($_SESSION['type'] == 'barista' or $_SESSION['type'] == 'manager' or $_SESSION['type'] == 'assistant'){
+                
+                       echo'<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                            data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
+                            aria-labelledby="offcanvasDarkNavbarLabel">
+                            <div class="offcanvas-header">
+                                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                <li class="nav-item"><a class="nav-link" href="staff/staff_dashboard.php">Staff Dashboard</a></li>
+                                <li class="nav-item" ><a class="nav-link" href="staff/staff_products.php">Products</a></li>'
+                    }
+                  .  if ($_SESSION['type'] == 'manager') {
+                             echo '<li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown">Managers
+                                    Tools</a>
+                                <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li><a class="dropdown-item" href="manager_view_users.php">All Staff</a>
+                                    </li>
+                                    <li><a class="dropdown-item"
+                                            href="transactions.php">Transactions</a>
+                                    </li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                </ul>
+                                </li>
+                                </ul>
+                                </div>';
                     }
                 ?>
-                
-                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
-                    aria-labelledby="offcanvasDarkNavbarLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
-                        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                            <!-- change these for  -->
-                            <?php
-                                if ($_SESSION['type'] == 'barista' or $_SESSION['type'] == 'manager' or $_SESSION['type'] == 'assistant'){
-                                    echo "<li class='nav-item'><a class='nav-link' href='staff/staff_dashboard.php'>Staff Dashboard</a></li>";
-                                    echo "<li class='nav-item' ><a class='nav-link' href='staff/staff_products.php'>Products</a></li>";
-                                }
-                                if ($_SESSION['type'] == 'manager') {
-                                    echo '<li class="nav-item"><a class="nav-link active" href="manager_dashboard.php">Home</a></li>
-                                        <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
-                                        <li class="nav-item dropdown">
-                                            <a class="nav-link dropdown-toggle" href="#" role="button"
-                                            data-bs-toggle="dropdown">Managers
-                                            Tools</a>
-                                        <ul class="dropdown-menu dropdown-menu-dark">
-                                            <li><a class="dropdown-item" href="manager_view_users.php">All Staff</a>
-                                            </li>
-                                            <li><a class="dropdown-item"
-                                                    href="transactions.php">Transactions</a>
-                                            </li>
-                                            <li>
-                                                <hr class="dropdown-divider">
-                                            </li>
-                                        <li><a class="dropdown-item" href="#">Preview</a></li>
-                                        </ul>
-                                        </li>
-                                        </ul>';
-                                }
-                            ?>
                             
                         </ul>
-                    </div>
+                    
                 </div>
+
             </div>
         </nav>
     </header>
